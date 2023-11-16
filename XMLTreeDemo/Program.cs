2af9351f5a164716df85e4c3eddae12077c31d88
Björn Bergdahl <84767875+BearBergdahl@users.noreply.github.com>
2023-11-16 14:38:43 +0100

    Completed code

--- XMLTreeDemo/Program.cs
+++ XMLTreeDemo/Program.cs
@@ -1,14 +1,42 @@
 ﻿using System.Xml;
+using System.Xml.Linq;
 
 namespace XMLTreeDemo
 {
     internal class Program
     {
+        static List<string> xmlnodes = new List<string>();
         static void Main(string[] args)
         {
             XmlDocument doc = new XmlDocument();
             doc.Load(".\\Breakfast.xml");
-        }    
+            XmlElement root = doc.DocumentElement;
+            SelectNodesOnAttribute(root);
+            TraverseNodes(doc.ChildNodes);
+        }
 
+        private static void TraverseNodes(XmlNodeList childNodes)
+        {
+            foreach (XmlNode node in childNodes)
+            {
+                List<string> temp = new List<string>();
+                temp.Add("Node name: " + node.Name.ToString());
+                XmlAttributeCollection xmlAttributes = node.Attributes;
+                if (xmlAttributes != null) 
+                {
+                    foreach (XmlAttribute attr in xmlAttributes)
+                    {
+                        temp.Add("Attribute: " + attr.Name +": "+ attr.Value);
+                    }
+                }
+                xmlnodes.AddRange(temp);
+                TraverseNodes(node.ChildNodes);
+            }
+        }
+
+        private static void SelectNodesOnAttribute(XmlElement? root)
+        {
+            XmlNode node = root.SelectSingleNode("//calories[@protein_percentage]");
+        }
     }
 }
