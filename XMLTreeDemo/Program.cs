10068df4c9fc498160529a17ea615c97e1fa21bb
Björn Bergdahl <84767875+BearBergdahl@users.noreply.github.com>
2023-11-16 08:55:16 +0100

    Add project files.

---
+++ XMLTreeDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml;
+
+namespace XMLTreeDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(".\\Breakfast.xml");
+        }    
+
+    }
+}
